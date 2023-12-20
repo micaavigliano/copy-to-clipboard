e1f13e20d7cf6dab846f6c0c49e66f55c3dee610
MicaAvigliano <micaela.avigliano@gmail.com>
2023-12-20 16:28:53 -0300

    create a reusable copy butoon

--- src/component/Copy.tsx
+++ src/component/Copy.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
 
 interface ICopyToClipboard {
-    text: string;
+  text: string;
 }
 
-const CopyToClipboard: React.FC<ICopyToClipboard> = (text) => {
-    return (
-        <>
-        <button>Copy!</button>
-        </>
-    )
-}
+const CopyToClipboard: React.FC<ICopyToClipboard> = ({ text }) => {
+  const handleCopyText = () => {
+    console.log(text);
+    navigator.clipboard.writeText(text!).then(
+      (success) => console.log("copied", success),
+      (error) => console.log("error", error)
+    );
+  };
+  return (
+    <>
+      <button onClick={handleCopyText}>Copy!</button>
+    </>
+  );
+};
+
+export default CopyToClipboard;
